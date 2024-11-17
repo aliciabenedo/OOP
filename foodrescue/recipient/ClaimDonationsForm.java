52f069a940e5f60bce21ab97ffeb8d407f2a8e44
aliciabenedo <116078757+aliciabenedo@users.noreply.github.com>
2024-11-17 20:23:59 +0000

    Add files via upload

---
+++ foodrescue/recipient/ClaimDonationsForm.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package foodrescue.recipient;
+
+/**
+ *
+ * @author NagbontianAliciaBene
+ */
+public class ClaimDonationsForm extends JFrame{
+    
+}
